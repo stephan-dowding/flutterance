751fa5906b4a606d20d0663b50689182d5db0570
Stephan Dowding <stephan.dowding@gmail.com>
2016-04-18 12:41:14 +0800

    neatening up chooseFragment

--- js/fragments/chooseFragment.js
+++ js/fragments/chooseFragment.js
@@ -17,62 +17,52 @@ this.ChooseFragment = (function() {
   };
 
   ChooseFragment.prototype.expandX = function(n, choices) {
-    if (n > choices.length) {
-      return [];
-    }
-    if (n === 0) {
-      return [''];
-    }
+    if (n > choices.length) { return []; }
+    if (n === 0) { return ['']; }
+
+    var result
     if (this.mode === 'left') {
-      return flatten(this.expandX(n - 1, choices.slice(1)).map(function(end) {
-        return choices[0].expand().map(function(start) {
-          return "" + start + end;
-        });
-      }));
+      result = this.expandX(n - 1, choices.slice(1))
+        .map(end => choices[0].expand()
+          .map(start => start + end)
+        );
     }
-    if (this.mode === 'right') {
-      return flatten(this.expandX(n - 1, choices.slice(0, -1)).map(function(start) {
-        return choices[choices.length - 1].expand().map(function(end) {
-          return "" + start + end;
-        });
-      }));
+    else if (this.mode === 'right') {
+      result = this.expandX(n - 1, choices.slice(0, -1))
+        .map(start => choices[choices.length - 1].expand()
+          .map(end => start + end)
+        );
     }
-    if (n === 1) {
-      return flatten(choices.map(function(choice) {
-        return choice.expand();
-      }));
+    else if (n === 1) {
+      result = choices.map(choice => choice.expand());
     }
-    if (this.mode !== 'unordered') {
-      return flatten(this.expandX(n, choices.slice(1)).concat(this.expandX(n - 1, choices.slice(1)).map(function(end) {
-        return choices[0].expand().map(function(start) {
-          return "" + start + end;
-        });
-      })));
+    else if (this.mode === 'unordered') {
+      result = choices.map((startElem, i) => this.expandX(n - 1, remove(choices, i))
+        .map(end => startElem.expand()
+          .map(start => start + end)
+        )
+      );
     }
-    return flatten(choices.map((function(_this) {
-      return function(startElem, i) {
-        return _this.expandX(n - 1, remove(choices, i)).map(function(end) {
-          return startElem.expand().map(function(start) {
-            return "" + start + end;
-          });
-        });
-      };
-    })(this)));
+    else {
+      result = this.expandX(n, choices.slice(1))
+        .concat(this.expandX(n - 1, choices.slice(1))
+          .map(end => choices[0].expand()
+            .map(start => start + end)
+          )
+        );
+    }
+
+    return flatten(result);
   };
 
   remove = function(arr, i) {
-    var temp;
-    temp = arr.slice(0);
+    var temp = arr.slice(0);
     temp.splice(i, 1);
     return temp;
   };
 
   flatten = function(arr) {
-    return arr.reduce(function(flat, toFlatten) {
-      var items;
-      items = Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten;
-      return flat.concat(items);
-    }, []);
+    return arr.reduce((flat, toFlatten) => flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten), []);
   };
 
   return ChooseFragment;
