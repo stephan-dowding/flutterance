58f4d444d5f8685792d1f37ca66437c1e7d020cb
Stephan Dowding <stephan.dowding@gmail.com>
2016-04-18 12:47:49 +0800

    neaten up literalParser

--- js/parsers/literalParser.js
+++ js/parsers/literalParser.js
@@ -3,9 +3,8 @@ var LiteralFragment;
 LiteralFragment = require('../fragments/literalFragment').LiteralFragment;
 
 this.parse = function(input) {
-  var c, i, j, len;
-  for (i = j = 0, len = input.length; j < len; i = ++j) {
-    c = input[i];
+  for (var i = 0; i < input.length; ++i) {
+    var c = input[i];
     if (c === '[' || c === '|' || c === ']') {
       return {
         fragment: new LiteralFragment(input.substring(0, i)),
