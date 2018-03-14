d28eb8fb8bbbb7941433da0c1389da8d887284e4
Josh Benner <bennerster@gmail.com>
2018-03-14 02:07:35 -0500

    Exporting to be used outside the CLI

--- js/run.js
+++ js/run.js
@@ -1,20 +1,28 @@
 #!/usr/bin/env node
 ;
-var flutterance;
+const flutterance = require('./flutterance');
 
-flutterance = require('./flutterance');
+flutterance.processPhrases = function(input_file, output_file){
+    if (!(input_file && output_file)) {
+        console.error("Flutterance requires an input and output file");
+        process.exit(1);
+    }
 
-if (!(process.argv[2] && process.argv[3])) {
-  console.error("Correct usage is:\n  flutterance <input-file> <output-file>");
-  process.exit(1);
+    flutterance.readFromFile(input_file).then(function(lines) {
+        var fragments, sentences;
+        fragments = flutterance.parseLines(lines);
+        sentences = flutterance.expandAll(fragments);
+        return flutterance.writeToFile(sentences, output_file);
+    })["catch"](function(error) {
+        console.error(error);
+        return process.exit(1);
+    });
 }
 
-flutterance.readFromFile(process.argv[2]).then(function(lines) {
-  var fragments, sentences;
-  fragments = flutterance.parseLines(lines);
-  sentences = flutterance.expandAll(fragments);
-  return flutterance.writeToFile(sentences, process.argv[3]);
-})["catch"](function(error) {
-  console.error(error);
-  return process.exit(1);
-});
+module.exports = flutterance;
+
+
+
+
+
+
